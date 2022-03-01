ba5c80385395b03364aa6e3eb9dcd2f91486df03
chsdwn <chasedown@hotmail.com>
2022-03-01 15:07:08 +0300

    Installs and enables ejs as the view engine. Enables static files. Adds static files to public folder. Splits header, navigation and footer codes as partials.

--- app.js
+++ app.js
@@ -2,5 +2,16 @@ const express = require('express')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.status(200).render('index', { page_name: 'index' })
+})
+app.get('/about', (req, res) => {
+  res.status(200).render('about', { page_name: 'about' })
+})
+
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
